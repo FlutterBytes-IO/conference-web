6c9fd6f8c701211f5fe50f404bcf28ec9211fa14
Yczar <babalolagbogo@gmail.com>
2022-09-20 21:19:59 +0100

    created: a test bloc for folder sampling

---
+++ lib/app/feature1/logic/bloc/test/test_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'test_event.dart';
+part 'test_state.dart';
+
+class TestBloc extends Bloc<TestEvent, TestState> {
+  TestBloc() : super(TestInitial()) {
+    on<TestEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
